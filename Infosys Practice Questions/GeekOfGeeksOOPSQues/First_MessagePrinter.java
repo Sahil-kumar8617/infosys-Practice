68a343e9da7090c1d803e81685cbad90c80b385d
Sahil Kumar <123888243+Sahil-kumar8617@users.noreply.github.com>
2025-08-12 09:53:53 +0530

    Add files via upload

---
+++ Infosys Practice Questions/GeekOfGeeksOOPSQues/First_MessagePrinter.java
@@ -0,0 +1,18 @@
+package GeekOfGeeksOOPSQues;
+import java.util.*;
+class Message{
+    public void printMessage(String name){
+        System.out.println("hello "+name);
+    }
+}
+
+
+public class First_MessagePrinter {
+    public static void main(String[] args) {
+        Message object=new Message();
+        object.printMessage("sahil");
+        
+        
+    }
+    
+}
